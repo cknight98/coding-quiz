0781dd1265472f1a04af62598f81b4a8e0ebeff2
Charlie Knight <cknight98@gmail.com>
2022-01-23 16:27:03 -0700

    added css styling and an array for questions and answers in javascript

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+let questions = 
+    [{
+        question: "If we want to use a dotted border around an image, which css property are we going to use?",
+        answer1: "border-line",
+        answer2: "border-decoration",
+        answer3: "border-color",
+        answer4correct: "border-style",
+    },
+    {
+        question: "Suppose we want to arrange three DIVs so that DIV 3 is placed above DIV1. Now, which CSS property are we going to use to control the stack order?",
+        answer1: "t-index",
+        answer2: "g-indez",
+        answer3: "p-index",
+        answer4correct: "z-index",
+    },
+    {
+        question: "If we want to place text around an image, which CSS property should we use?",
+        answer1: "push",
+        answer2: "align",
+        answer3: "wrap",
+        answer4correct: "float",
+    },
+    {
+        question: "An ordered list can be represented by ______.",
+        answer1: "<ul>",
+        answer2: "<li>",
+        answer3: "<uo>",
+        answer4correct: "<ol>",
+    },
+    {
+        question: "Which of the following HTML tags will insert a line break?",
+        answer1: "<p>",
+        answer2: "<break>",
+        answer3: "<line>",
+        answer4correct: "<br>",
+    }]
